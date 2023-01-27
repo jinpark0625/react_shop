bbced277576184392804b16edd39b14820b5aeb7
jin park <jinpark0625@gmail.com>
2023-01-27 17:27:45 -0500

    Add pages

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllProducts = () => {
+  return <div>AllProducts</div>;
+};
+
+export default AllProducts;
---
+++ src/pages/MyCart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyCart = () => {
+  return <div>My Cart</div>;
+};
+
+export default MyCart;
