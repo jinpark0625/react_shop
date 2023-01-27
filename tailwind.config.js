8f4183ef74b3d493ad3ed177dfed4a8cba36f669
jin park <jinpark0625@gmail.com>
2023-01-27 17:21:59 -0500

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"], // 이부분 수정
+  content: ['./src/**/*.{js,jsx}'], // 이부분 수정
   theme: {
-    extend: {},
+    extend: {
+      // custom colors
+      colors: {
+        brand: '#f96262',
+      },
+    },
   },
   plugins: [],
 };
