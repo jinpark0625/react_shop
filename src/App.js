2e3878af63d7ab11d8f36f0661314bb29fc5d616
jin park <jinpark0625@gmail.com>
2023-01-27 17:31:32 -0500

    Changed name of navbar file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
-import NavBar from "./components/navBar";
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <NavBar />
+      <Navbar />
       <Outlet />
     </>
   );
