1249fd9a567468586c61f091a921fc7e24f4f89b
jin park <jinpark0625@gmail.com>
2023-01-30 01:52:24 -0500

    Add ts & tsx content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'], // 이부분 수정
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // 이부분 수정
   theme: {
     extend: {
       // custom colors
