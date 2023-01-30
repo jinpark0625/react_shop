3b1fb91007c360fc3e6bf844ce28dfd65d41f785
jin park <jinpark0625@gmail.com>
2023-01-30 01:55:23 -0500

    Add typescript to react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
