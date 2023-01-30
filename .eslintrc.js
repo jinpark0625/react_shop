197671873d4c1e8d362bbdf4b124bb0996d7f698
jin park <jinpark0625@gmail.com>
2023-01-30 01:51:26 -0500

    Setup for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
@@ -16,10 +17,34 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'tailwindcss/classnames-order': 'off',
     'tailwindcss/no-custom-classname': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': [
+      'warn',
+      {
+        allowNullableObject: true,
+      },
+    ],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'off',
+    'no-callback-literal': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/consistent-indexed-object-style': 'off',
   },
 };
