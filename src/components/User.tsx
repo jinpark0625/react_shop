36f95852aca1b9b718a949efca62bd7e28f5194c
jin park <jinpark0625@gmail.com>
2023-01-31 01:00:26 -0500

    Updat CSS styles

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,7 +2,7 @@ import { UserData } from '../utils/interfaces';
 
 export default function User({ displayName, photoURL }: UserData) {
   return (
-    <div className="flex items-center">
+    <div className="flex shrink-0 items-center">
       <img
         className="mr-2 h-10 w-10 rounded-full"
         src={photoURL}
