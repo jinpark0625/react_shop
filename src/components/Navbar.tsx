f9205739c8b24e7f5ef6a2ceb54697e5366ef1ad
jin park <jinpark0625@gmail.com>
2023-01-31 17:05:05 -0500

    Import context data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,15 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
-import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
-import { UserData } from '../utils/interfaces';
 import Button from './ui/Button';
-
-type UserDataType = UserData | null;
+import { useAuthContext } from './context/AuthContext';
 
 export default function Navbar() {
-  const [user, setUser] = useState<UserDataType>();
-
-  useEffect(() => {
-    onUserStateChange(setUser);
-  }, []);
+  const { ...contextData } = useAuthContext();
+  const { user, login, logout } = contextData;
+  const userData = user ?? null;
+  const isAdmin = userData ? 'isAdmin' in userData : null;
 
   return (
     <header className="flex justify-between border-b border-gray-300 p-2 ">
@@ -24,18 +19,15 @@ export default function Navbar() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <BsFillPencilFill />
-        </Link>
-        {Boolean(user?.isAdmin) && (
+        {userData && <Link to="/carts">Carts</Link>}
+        {Boolean(isAdmin) && (
           <Link to="/products/new" className="text-2xl">
             <BsFillPencilFill />
           </Link>
         )}
-        {user && <User {...user} />}
-        {!user && <Button onClick={login} text={'Login'} />}
-        {user && <Button onClick={logout} text={'Logout'} />}
+        {userData && <User {...userData} />}
+        {!userData && <Button onClick={login} text={'Login'} />}
+        {userData && <Button onClick={logout} text={'Logout'} />}
       </nav>
     </header>
   );
