5d6add31026a9d3a796cf73d1fbfeb3a0c5c1467
jin park <jinpark0625@gmail.com>
2023-01-31 17:02:43 -0500

    Add context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { AuthContextProvider } from 'components/context/AuthContext';
 
 export default function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
