216d9b770bc5fe5aee13634d04c062026d055f6f
jin park <jinpark0625@gmail.com>
2023-01-31 17:06:01 -0500

    Add Nullish coalescing operator for src & alt

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,12 +1,12 @@
-import { UserData } from '../utils/interfaces';
+import { User as UserType } from 'firebase/auth';
 
-export default function User({ displayName, photoURL }: UserData) {
+export default function User({ displayName, photoURL }: UserType) {
   return (
     <div className="flex shrink-0 items-center">
       <img
         className="mr-2 h-10 w-10 rounded-full"
-        src={photoURL}
-        alt={displayName}
+        src={photoURL ?? ''}
+        alt={displayName ?? ''}
       />
       <span className="hidden md:block">{displayName}</span>
     </div>
