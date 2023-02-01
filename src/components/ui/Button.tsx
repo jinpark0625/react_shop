1ebfdfe829e6b9e3846dd85f2d9977959ac21277
jin park <jinpark0625@gmail.com>
2023-01-31 23:07:40 -0500

    Add disable type

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,15 @@
 interface ButtonPros {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
-export default function Button({ text, onClick }: ButtonPros) {
+export default function Button({ text, onClick, disabled }: ButtonPros) {
   return (
     <button
       onClick={onClick}
       className="rounded-sm bg-brand py-2 px-4 text-white hover:brightness-110"
+      disabled={disabled}
     >
       {text}
     </button>
