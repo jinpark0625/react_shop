e9cf3c8122b917f3e49f99713e62e29e8c31a391
jin park <jinpark0625@gmail.com>
2023-02-01 02:23:47 -0500

    Add query provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthContextProvider } from 'components/context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
