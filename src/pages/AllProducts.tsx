447abd3a4abd5328b86c2ebf53ed40c26d72421f
jin park <jinpark0625@gmail.com>
2023-02-01 02:25:31 -0500

    Add products

--- src/pages/AllProducts.tsx
+++ src/pages/AllProducts.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Products from 'components/Products';
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  return <Products />;
 };
 
 export default AllProducts;
