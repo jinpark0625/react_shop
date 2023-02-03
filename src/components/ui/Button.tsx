37dcbb74d0931b5277813b3ee2a62798cdc015d8
jin park <jinpark0625@gmail.com>
2023-02-03 01:21:23 -0500

    Update UI design by modifying CSS styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ text, onClick, disabled }: ButtonPros) {
   return (
     <button
       onClick={onClick}
-      className="rounded-sm bg-brand py-2 px-4 text-white hover:brightness-110"
+      className="rounded-full bg-neutral-800 py-2 px-4 text-white hover:brightness-110"
       disabled={disabled}
     >
       {text}
