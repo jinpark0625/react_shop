21306258d345d12af48d4658a8bc76bc963410da
jin park <jinpark0625@gmail.com>
2023-02-03 01:23:15 -0500

    Add random color & toast

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,10 +1,19 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Button from '../components/ui/Button';
 import { SelectedProductType } from '../utils/interfaces';
 import useCart from '../hooks/useCart';
 import { useAuthContext } from '../context/AuthContext';
 import { toast } from 'react-toast';
+
+const colors = [
+  '#957570',
+  '#a1a68e',
+  '#bd7c4a',
+  '#c2a56d',
+  '#89a3b4',
+  '#b294b0',
+];
 
 const ProductDetail = () => {
   const { state } = useLocation();
@@ -21,8 +30,13 @@ const ProductDetail = () => {
   const { id, image, title, description, category, price, options } =
     state.product;
 
-  const [success, setSuccess] = useState<string | null>(null);
+  // const [success, setSuccess] = useState<string | null>(null);
   const [selected, setSelected] = useState(options?.[0]);
+  const [bgColor, setBgColor] = useState<string>('#fff');
+
+  useEffect(() => {
+    setBgColor(colors[Math.floor(Math.random() * colors.length)]);
+  }, []);
 
   const handleSelect = (e: ChangeEvent<HTMLSelectElement>) =>
     setSelected(e.target.value);
@@ -46,26 +60,32 @@ const ProductDetail = () => {
 
     addOrUpdateItem.mutate(product, {
       onSuccess: () => {
-        setSuccess('장바구니에 추가되었습니다.');
-        setTimeout(() => setSuccess(null), 3000);
+        toast.success('The product has been added successfully.', {
+          backgroundColor: '#008000',
+        });
       },
     });
   };
 
   return (
     <>
-      <p className="mx-12 mt-4 text-gray-700">{category}</p>
-      <section className="flex flex-col p-4 md:flex-row">
-        <img className="w-full basis-7/12 px-4" src={image} alt={title} />
-        <div className="flex w-full basis-5/12 flex-col p-4">
+      <p className="m-auto  mt-4 max-w-screen-2xl text-gray-700">{category}</p>
+      <section className="m-auto flex max-w-screen-2xl flex-col p-4 md:flex-row">
+        <div
+          className="mr-8 basis-6/12 rounded-3xl px-4"
+          style={{ backgroundColor: bgColor }}
+        >
+          <img className="w-full" src={image} alt={title} />
+        </div>
+        <div className="flex w-full basis-6/12 flex-col p-4">
           <h2 className=" py-2 text-3xl font-bold">{title}</h2>
           <p className="border-b border-gray-400 py-2 text-2xl font-bold">
             ${price}
           </p>
           <p className="py-4 text-lg">{description}</p>
-          <div className="flex items-center">
+          <div className="mb-4 flex items-center">
             <label htmlFor="select" className="font-bold text-brand">
-              옵션 :
+              Options :
             </label>
             <select
               className="m-4 flex-1 border-2 border-dashed border-brand p-2 outline-none"
@@ -78,8 +98,8 @@ const ProductDetail = () => {
               ))}
             </select>
           </div>
-          {success && <p className="my-2">{success}</p>}
-          <Button text="장바구니에 추가" onClick={handleClick} />
+          {/* {success && <p className="my-2">{success}</p>} */}
+          <Button text="Add cart" onClick={handleClick} />
         </div>
       </section>
     </>
