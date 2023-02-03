75b0b8b67dbfb82a2510bd9c50b4b9af27743a70
jin park <jinpark0625@gmail.com>
2023-02-03 01:21:48 -0500

    Update UI design by modifying CSS styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import User from './User';
 import Button from './ui/Button';
 import { useAuthContext } from '../context/AuthContext';
 import CartStatus from './CartStatus';
+import { AiFillGithub } from 'react-icons/ai';
+import { SiPrestashop } from 'react-icons/si';
 
 export default function Navbar() {
   const { ...contextData } = useAuthContext();
@@ -13,12 +14,19 @@ export default function Navbar() {
   const isAdmin = userData ? 'isAdmin' in userData : null;
 
   return (
-    <header className="flex justify-between border-b border-gray-300 p-2 ">
-      <Link to="/" className="flex items-center text-4xl text-brand">
-        <FiShoppingBag />
-        <h1>Shoppy</h1>
+    <header className="m-auto grid max-w-screen-2xl grid-cols-[45%_10%_45%] border-b border-gray-300 p-5">
+      <div className="flex items-center">
+        <Link to="/" className="flex text-2xl">
+          <AiFillGithub />
+        </Link>
+      </div>
+      <Link
+        to="/"
+        className="flex items-center justify-center text-5xl text-brand"
+      >
+        <SiPrestashop />
       </Link>
-      <nav className="flex items-center gap-4 font-semibold">
+      <nav className="flex items-center justify-end gap-4 font-semibold">
         <Link to="/products">Products</Link>
         {userData && (
           <Link to="/carts">
