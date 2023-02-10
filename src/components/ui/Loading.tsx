7efcd273702cc25348ae9dbd9fc76a4cf80ba1ea
jin park <jinpark0625@gmail.com>
2023-02-10 13:06:30 -0500

    Add loading screen

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,13 @@
+import { CgSpinner } from 'react-icons/cg';
+
+export default function Loading() {
+  return (
+    <div className="relative z-10">
+      <div className="fixed inset-0 bg-gray-500/20 transition-opacity">
+        <div className="flex h-screen items-center justify-center">
+          <CgSpinner className="animate-spin text-3xl text-indigo-600" />
+        </div>
+      </div>
+    </div>
+  );
+}
