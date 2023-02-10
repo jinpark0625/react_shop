6acb655ed4cb45ab488afa244a8b225d6b4d0fdb
jin park <jinpark0625@gmail.com>
2023-02-10 13:07:35 -0500

    Add default image

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -5,8 +5,12 @@ export default function User({ displayName, photoURL }: UserType) {
     <div className="flex shrink-0 items-center">
       <img
         className="mr-2 h-10 w-10 rounded-full"
-        src={photoURL ?? ''}
-        alt={displayName ?? ''}
+        src={
+          photoURL === 'default' || !photoURL
+            ? '/images/default_image.webp'
+            : photoURL
+        }
+        alt={displayName ?? 'Anonymous'}
       />
       <span className="hidden md:block">{displayName}</span>
     </div>
