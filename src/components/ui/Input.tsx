9be64eff303733655e71f952c05fd35d0ef14821
jin park <jinpark0625@gmail.com>
2023-02-10 13:06:18 -0500

    Add input

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  labelText?: string;
+  className?: string;
+  ariaInvalid?: boolean;
+  type: string;
+  error?: string;
+}
+
+const LABEL_CLASS = `duration absolute top-px left-2 -translate-y-1/2 scale-75 bg-white px-1 transition-all origin-left	
+peer-placeholder-shown:top-7 peer-placeholder-shown:left-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-slate-500
+peer-focus:top-px peer-focus:left-2 peer-focus:scale-75 peer-focus:text-sky-500 pointer-events-none	`;
+
+const Input = React.forwardRef<HTMLInputElement, IProps>(
+  ({ labelText, className, type, ariaInvalid, error, ...props }, ref) => {
+    return (
+      <>
+        <div
+          className={`${error ? 'animate-shake' : ''} ${
+            className ?? ''
+          } relative`}
+        >
+          <input
+            ref={ref}
+            {...props}
+            className={`peer w-full rounded-lg ${
+              error ? 'border-red-500' : ''
+            }`}
+            type={type}
+            aria-invalid={!ariaInvalid ? undefined : error ? 'true' : 'false'}
+            placeholder=" "
+          />
+          <label className={LABEL_CLASS}>{labelText}</label>
+          {error && (
+            <small
+              role="alert"
+              className={`${error ? 'animate-shake' : ''} text-red-500`}
+            >
+              {error}
+            </small>
+          )}
+        </div>
+      </>
+    );
+  },
+);
+
+Input.displayName = 'Input';
+
+export default Input;
