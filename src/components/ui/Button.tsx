bc6a4a1e29ba5acf13d74d215599a4bd82c72e4a
jin park <jinpark0625@gmail.com>
2023-02-10 13:05:49 -0500

    Add icons & styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,30 @@
+import React from 'react';
 interface ButtonPros {
   text: string;
   onClick?: () => void;
   disabled?: boolean;
+  className?: string;
+  icon?: IconType;
 }
 
-export default function Button({ text, onClick, disabled }: ButtonPros) {
+type IconType = () => JSX.Element;
+
+export default function Button({
+  text,
+  onClick,
+  disabled,
+  className,
+  icon,
+}: ButtonPros) {
   return (
     <button
       onClick={onClick}
-      className="rounded-full bg-neutral-800 py-2 px-4 text-white hover:brightness-110"
+      className={`flex items-center justify-center text-white hover:brightness-110 ${
+        className ?? 'rounded-full bg-neutral-800 py-2 px-4'
+      }`}
       disabled={disabled}
     >
+      {icon && React.createElement(icon)}
       {text}
     </button>
   );
