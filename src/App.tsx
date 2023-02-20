9d5ff9f462df1bce94eb1a7cbc33955566b64a18
jin park <jinpark0625@gmail.com>
2023-02-19 22:49:29 -0500

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar';
 import { AuthContextProvider } from 'context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toast';
+import Footer from 'components/layout/Footer';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
@@ -14,6 +19,7 @@ export default function App() {
       <AuthContextProvider>
         <Navbar />
         <Outlet />
+        <Footer />
         <ToastContainer delay={3000} position="top-right" />
       </AuthContextProvider>
       {/* <ReactQueryDevtools initialIsOpen={true} /> */}
