d29e105ac9339fa49a277d3442074d74836c305f
JIN PARK <69961780+jinpark0625@users.noreply.github.com>
2023-02-20 00:38:48 -0500

    Delete Banner.tsx

--- src/components/Banner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Banner() {
-  return (
-    <section className="relative h-[32rem] bg-yellow-900">
-      <div className="h-full w-full bg-banner bg-cover"></div>
-    </section>
-  );
-}
