8acfbaf324010c165e6dd8c54b9923dae2a27f88
jin park <jinpark0625@gmail.com>
2023-02-20 01:24:49 -0500

    feat: Add data to layoutData.ts for NavBar and Footer component

---
+++ src/data/layout/layoutData.ts
@@ -0,0 +1,116 @@
+// Header Data
+export const headerData = {
+  categories: [
+    {
+      id: 'Products',
+      name: 'Products',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc:
+            'https://res.cloudinary.com/dchh1a7k8/image/upload/v1676699141/items/whiteTote_lwcm2h.webp',
+          imageAlt: 'Tote bag',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc:
+            'https://res.cloudinary.com/dchh1a7k8/image/upload/v1676699141/items/blackT_front_ro4ws2.webp',
+          imageAlt: 'black tees',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [{ name: 'Tops', href: '#' }],
+        },
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [{ name: 'Bags', href: '#' }],
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'NFTs', href: '/' },
+    { name: 'Shop All', href: '/' },
+  ],
+};
+
+// Footer Data
+export const footerTerms = [
+  {
+    name: 'Terms',
+    link: '/',
+  },
+  {
+    name: ' Privacy Policy',
+    link: '/',
+  },
+];
+
+export const footerMarketPlace = [
+  {
+    name: 'All NFTs',
+    link: '/',
+  },
+  {
+    name: 'Products',
+    link: '/',
+  },
+];
+
+export const footerResource = [
+  {
+    name: 'Documentation',
+    link: '/',
+  },
+  {
+    name: 'Help Center',
+    link: '/',
+  },
+  {
+    name: 'Blog',
+    link: '/',
+  },
+  {
+    name: 'Support Center',
+    link: '/',
+  },
+];
+
+export const footerCompany = [
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'About us',
+    link: '/',
+  },
+  {
+    name: 'Privacy Policy',
+    link: '/',
+  },
+];
+
+export const footerSocial = [
+  {
+    path: 'M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z',
+    link: '/',
+    name: 'Twitter',
+  },
+  {
+    path: 'M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z',
+    link: '/',
+    name: 'Github',
+  },
+  {
+    path: 'M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z',
+    link: '/',
+    name: 'Facebook',
+  },
+];
