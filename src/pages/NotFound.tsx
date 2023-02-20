903931c4720ead6a2baf40dff91a3366df13ac0d
jin park <jinpark0625@gmail.com>
2023-02-20 00:30:23 -0500

    feat: Add custom styling and content to NotFound page
    - Updated the NotFound page with custom styling and content to provide a better user experience.
    - This includes a new image, page title, and error message, as well as a "Go back home" button that redirects the user to the home page.
    - This change should make it easier for users to understand that they have landed on an error page and provide them with a clear path to return to the home page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import { Link } from 'react-router-dom';
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <main className="flex h-screen items-center justify-center bg-white py-24 px-6 sm:py-32 lg:px-8 ">
+      <div className="text-center">
+        <img
+          alt="404 image"
+          src="/images/404.webp"
+          className="w-full max-w-sm"
+        />
+        <h1 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-all hover:bg-sky-600 hover:shadow-md hover:shadow-sky-600"
+          >
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
