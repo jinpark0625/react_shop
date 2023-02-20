af030d7ba499277168205516f21bf54298b90abf
jin park <jinpark0625@gmail.com>
2023-02-20 01:27:56 -0500

    feat: Add break points for main page carousel

---
+++ src/data/Home/slideOptions.ts
@@ -0,0 +1,32 @@
+export const PRODUCT_BREAK_POINTS = {
+  576: {
+    slidesPerView: 2,
+    spaceBetween: 30,
+  },
+  990: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+  1300: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+  },
+};
+
+export const NFT_BREAK_POINTS = {
+  576: {
+    slidesPerView: 2,
+    spaceBetween: 30,
+  },
+  990: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+  1300: {
+    slidesPerView: 4,
+    spaceBetween: 40,
+  },
+};
+
+export const PRODUCT_LOADING_ARRAY = [1, 2, 3];
+export const NFT_LOADING_ARRAY = [1, 2, 3, 4];
