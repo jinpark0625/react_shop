7f0bdc607f4a6bbc30aeb8717af361022d6a789c
jin park <jinpark0625@gmail.com>
2023-03-13 18:15:37 -0400

    feat: Add useIntersect hook
    - Used the IntersectionObserver API to trigger a callback function when an element intersects with the viewport

---
+++ src/hooks/useIntersect.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useRef } from 'react';
+import {
+  FetchNextPageOptions,
+  InfiniteQueryObserverResult,
+} from '@tanstack/react-query';
+
+import { InfiniteReturnType } from 'utils/interfaces';
+
+const useIntersect = (
+  callback: IntersectionObserverCallback,
+  hasNextPage?: boolean,
+  fetchNextPage?: (
+    options?: FetchNextPageOptions | undefined,
+  ) => Promise<InfiniteQueryObserverResult<InfiniteReturnType, Error>>,
+) => {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const ref = useCallback(
+    (node: HTMLLIElement) => {
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver(callback, {
+        threshold: 1,
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [callback, hasNextPage, fetchNextPage],
+  );
+
+  return ref;
+};
+
+export default useIntersect;
