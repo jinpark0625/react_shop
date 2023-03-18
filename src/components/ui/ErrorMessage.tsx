40ac6ac7c119af32f2e160057c3ea6b236411016
jin park <jinpark0625@gmail.com>
2023-03-18 19:39:27 -0400

    feat: Add reusable error message component

---
+++ src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => {
+  return (
+    <p className="col-span-2 text-center text-red-500">
+      Sorry something went wrong. Please try again
+    </p>
+  );
+};
+
+export default ErrorMessage;
