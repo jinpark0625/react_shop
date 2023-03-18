d8e66d677c6b5f6f778343f4b5f186a0a7a8e263
jin park <jinpark0625@gmail.com>
2023-03-18 19:46:23 -0400

    feat: Add Link and id props

--- src/components/ui/NftCard.tsx
+++ src/components/ui/NftCard.tsx
@@ -1,31 +1,28 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from 'react-router-dom';
 
 interface IProps {
   image: string;
   title: string;
+  id: number;
 }
 
-export default function NftCard({ image, title }: IProps) {
+export default function NftCard({ image, title, id }: IProps) {
   return (
-    <>
-      <div className="relative overflow-hidden rounded-2xl bg-gray-100 pt-[100%]">
-        <div
-          className="absolute 
-      inset-0 translate-x-1/2
-      translate-y-1/2
-      "
-        >
+    <Link to={`/nfts/${id}`}>
+      <div className="relative isolate overflow-hidden rounded-2xl bg-gray-100 pt-[100%]">
+        <div className="absolute inset-0 translate-x-1/2 translate-y-1/2">
           <LazyLoadImage
             src={image}
             alt={title}
             effect="blur"
-            className="absolute top-0 left-0 h-full w-auto -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:opacity-75"
+            className=" absolute top-0 left-0 h-full w-auto -translate-x-1/2 -translate-y-1/2 rounded-2xl transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:opacity-75"
           />
         </div>
       </div>
       <div className="p-5">
         <h3 className="text-center text-lg font-bold text-gray-900">{title}</h3>
       </div>
-    </>
+    </Link>
   );
 }
