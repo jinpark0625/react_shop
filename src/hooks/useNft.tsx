94d9d2b6711801ce4eef91e0d434774a805a412a
jin park <jinpark0625@gmail.com>
2023-03-17 22:29:03 -0400

    refactor: Update useNft hook to use Supabase API and add pagination
    - Previously used Firebase API to fetch NFT data
    - Refactored the hook to use Supabase API instead, using the `fetchNFTAll` and `fetchNFT` functions provided in the `api/supabase` module
    - Added pagination functionality using the `useInfiniteQuery` hook from the `react-query` library.

--- src/hooks/useNft.tsx
+++ src/hooks/useNft.tsx
@@ -1,15 +1,47 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import { getNFTs } from 'api/firebase';
-import { NftType } from '../utils/interfaces';
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryResult,
+  useQuery,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { InfiniteReturnType, NftType } from '../utils/interfaces';
+import { ParsedQuery } from 'query-string';
+import { fetchNFTAll, fetchNFT } from 'api/supabase';
 
-export default function useNft() {
-  const nftsQuery: UseQueryResult<NftType[], Error> = useQuery(
-    ['nfts'],
-    async () => await getNFTs('NFT'),
+export default function useNft(query: ParsedQuery<string>) {
+  const nftsAllData: UseQueryResult<NftType[], Error> = useQuery(
+    ['nftsAll'],
+    async () => await fetchNFTAll(),
     {
       staleTime: 1000 * 60,
     },
   );
 
-  return { nftsQuery };
+  const nftsQuery: UseInfiniteQueryResult<InfiniteReturnType, Error> =
+    useInfiniteQuery(
+      ['nfts', query],
+      async ({ pageParam = 0 }) => {
+        const {
+          props: { data, page },
+        } = await fetchNFT({
+          query,
+          pageParam,
+        });
+
+        if (data && data.length < 10) return { data, nextPage: undefined };
+
+        return {
+          data,
+          nextPage: Number(page) + 1,
+        };
+      },
+      {
+        getNextPageParam: (pageParam) => {
+          return pageParam.nextPage ?? undefined;
+        },
+        staleTime: 1000 * 60,
+      },
+    );
+
+  return { nftsAllData, nftsQuery };
 }
