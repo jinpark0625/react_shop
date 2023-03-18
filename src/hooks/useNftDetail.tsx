27f35062d6ff5d1c4fd470553399b8597f5f6e90
jin park <jinpark0625@gmail.com>
2023-03-17 23:34:23 -0400

    feat: Add useNftDetail hook to handle API data
    - Used the Supabase API to fetch NFT data and then passes that data to the OpenSea API to get more NFT data
    - Returned a data object that includes the NFT data, the permalink, and the ETH and USD prices

---
+++ src/hooks/useNftDetail.tsx
@@ -0,0 +1,36 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { NftType } from 'utils/interfaces';
+import { fetchSelectedNFT } from 'api/supabase';
+import opensea from 'api/opensea';
+
+interface ReturnDataType {
+  data: NftType[];
+  permalink: string;
+  eth_price: number;
+  usd_price: number;
+}
+
+export default function useNftDetail(id: string) {
+  const selectedNftQuery: UseQueryResult<ReturnDataType, Error> = useQuery(
+    ['nft', id],
+    async () => {
+      const data = await fetchSelectedNFT(id);
+      const {
+        data: {
+          permalink,
+          collection: {
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            payment_tokens: [{ eth_price, usd_price }],
+          },
+        },
+      } = await opensea(data?.[0].token_id ?? '');
+
+      return { data, permalink, eth_price, usd_price };
+    },
+    {
+      staleTime: 1000 * 60,
+    },
+  );
+
+  return selectedNftQuery;
+}
