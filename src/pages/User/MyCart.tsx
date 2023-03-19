9332c96fbb09f441f651ded05f30d12f92f7d653
jin park <jinpark0625@gmail.com>
2023-03-19 00:44:36 -0400

    refactor: Improve MyCart component
    - Removed unused imports and variables
    - Added new imports to support new components and features
    - Added loading and error states to improve UX
    - Added tax calculation for products
    - Changed the layout of the component

--- src/pages/User/MyCart.tsx
+++ src/pages/User/MyCart.tsx
@@ -1,55 +1,99 @@
 import CartItem from '../../components/CartItem';
-import { BsFillPlusCircleFill } from 'react-icons/bs';
-import { FaEquals } from 'react-icons/fa';
 import PriceCard from '../../components/PriceCard';
 import Button from '../../components/ui/Button';
 import { SelectedProductType } from '../../utils/interfaces';
 import useCart from '../../hooks/useCart';
+import ProductTitle from 'components/ui/ProductTitle';
+import LoadingSkeleton from 'components/ui/LoadingSkeleton';
+import { useAuthContext } from 'context/AuthContext';
+import NotFound from 'pages/NotFound';
 
-const SHIPPING = 3000;
+const SHIPPING = 5;
 
 const MyCart = () => {
+  const { ...contextData } = useAuthContext();
+  const { user } = contextData;
+
   const {
     cartQuery: { isLoading, error, data: products },
   } = useCart();
 
-  if (isLoading) return <p>Loading..</p>;
-  if (error) return <p>{error.toString()}</p>;
-
   const hasProducts = products && products.length > 0;
+
+  const taxPrice = products?.reduce(
+    (prev: number, current: SelectedProductType) =>
+      prev + current.price * current.quantity * 0.13,
+    0,
+  );
   const totalPrice = products?.reduce(
     (prev: number, current: SelectedProductType) =>
       prev + current.price * current.quantity,
     0,
   );
 
+  if (!user) return <NotFound />;
+
+  if (isLoading)
+    return (
+      <div className="mx-auto mb-32 w-full max-w-7xl px-4 pt-24 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-6">
+          <div className="col-span-4 mb-8 pr-0 lg:pr-10">
+            <LoadingSkeleton className=" animate-pulse rounded-md bg-slate-50  pt-[30%]" />
+            <LoadingSkeleton className="mt-10 animate-pulse rounded-md bg-slate-50  pt-[30%]" />
+          </div>
+          <div className="relative col-span-2 w-full animate-pulse overflow-hidden rounded-2xl bg-slate-50  pt-[100%]">
+            <LoadingSkeleton className="absolute top-0 left-0 h-full w-auto -translate-x-1/2 -translate-y-1/2 " />
+          </div>
+        </div>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="mx-auto max-w-7xl px-4 pt-24 text-center text-red-500 sm:px-6 lg:px-8">
+        <p>Sorry something went wrong. Please try agaain</p>
+      </div>
+    );
+
   return (
-    <section className="m-auto flex max-w-screen-2xl flex-col p-8">
-      <p className="border-b border-gray-300 pb-8 text-center text-2xl font-bold">
-        My Cart
-      </p>
+    <section className="mx-auto mb-32 w-full max-w-7xl px-4 pt-24 sm:px-6 lg:px-8">
+      <ProductTitle title="My Cart" />
       {!hasProducts && (
-        <p className="mt-12 text-center text-lg">No products in the cart.</p>
+        <p className="mt-32 text-center text-lg">No products in the cart.</p>
       )}
       {hasProducts && (
-        <>
-          <ul className="mb-8 border-b border-gray-300 p-4 px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-6">
+          <ul className="col-span-4 mb-8 py-4 pr-0 lg:pr-10">
             {products?.map((product) => (
               <CartItem key={product.id} product={product} />
             ))}
           </ul>
-          <div className="mb-8 flex items-center justify-between px-2 md:px-8 lg:px-16">
-            <PriceCard text="Product" price={totalPrice} />
-            <BsFillPlusCircleFill className="shrink-0" />
-            <PriceCard text="Shipping fee" price={SHIPPING} />
-            <FaEquals className="shrink-0" />
-            <PriceCard
-              text="Total"
-              price={totalPrice ? totalPrice + SHIPPING : SHIPPING}
+          <div className="col-span-2 mt-4 mb-8 flex flex-col pl-0 lg:pl-10">
+            <h3 className="mb-4 text-xl font-medium text-gray-900">
+              Order summary
+            </h3>
+            <div>
+              <PriceCard text="Subtotal" price={totalPrice} />
+              <PriceCard text="Shipping estimate" price={SHIPPING} />
+              <PriceCard
+                text="Tax estimate"
+                price={taxPrice ? taxPrice?.toFixed(2) : 0}
+              />
+              <PriceCard
+                total
+                text="Order total"
+                price={
+                  totalPrice
+                    ? totalPrice + SHIPPING + (taxPrice ?? 0)
+                    : SHIPPING
+                }
+              />
+            </div>
+            <Button
+              text="Checkout"
+              className="mt-4 flex w-full items-center justify-center rounded-md border border-transparent bg-violet-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-violet-600"
             />
           </div>
-          <Button text="Order" />
-        </>
+        </div>
       )}
     </section>
   );
