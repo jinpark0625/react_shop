58e019f56f8c96600edccc85f4b9e5dc9e15d7a4
jin park <jinpark0625@gmail.com>
2023-03-18 21:58:03 -0400

    refactor: Improve code readability and user experience
    - Remove unused imports and simplify code structure
    - Introduce a dropdown to select product quantity

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,11 +1,12 @@
-// import { addOrUpdateToCart, removeFromCart } from '../api/firebase';
-import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
-import { RiDeleteBin5Fill } from 'react-icons/ri';
 import { SelectedProductType } from '../utils/interfaces';
 import useCart from '../hooks/useCart';
-
-const ICON_CLASS =
-  'hovber:text-brand mx-1 cursor-pointer transition-all hover:scale-105';
+import {
+  ChevronDownIcon,
+  TrashIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { ChangeEvent, useState } from 'react';
 
 interface ProductProps {
   product: SelectedProductType;
@@ -13,34 +14,63 @@ interface ProductProps {
 
 export default function CartItem({
   product,
-  product: { id, image, title, option, quantity, price },
+  product: { id, image, title, quantity, size, price },
 }: ProductProps) {
   const { addOrUpdateItem, removeItem } = useCart();
-  const handleMinus = () => {
-    if (quantity < 2) return;
-    addOrUpdateItem.mutate({ ...product, quantity: quantity - 1 });
+
+  const [selected, SetSelected] = useState(quantity);
+
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const quantity = Number(e.target.value);
+    SetSelected(quantity);
+    addOrUpdateItem.mutate({ ...product, quantity });
   };
-  const handlePlus = () => {
-    addOrUpdateItem.mutate({ ...product, quantity: quantity + 1 });
-  };
+
   const handleDelete = () => {
     removeItem.mutate(id);
   };
 
   return (
-    <li className="my-2 flex items-center justify-between">
-      <img src={image} alt={title} className="w-24 rounded-lg md:w-48" />
-      <div className="ml-4 flex flex-1 justify-between">
-        <div className="basis-3/5">
+    <li className="flex justify-between border-t border-gray-200 py-5 ">
+      <div className="h-20 w-20 shrink-0 rounded-lg bg-gray-100 sm:h-[136px] sm:w-[136px] md:h-40 md:w-40">
+        <LazyLoadImage
+          src={image && Array.isArray(image) ? image[0] : image}
+          alt={title}
+          effect="blur"
+          className="h-full w-auto transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:opacity-75"
+        />
+      </div>
+      <div className="ml-4 flex w-full flex-wrap md:grid md:grid-cols-3">
+        <div className="w-full">
           <p className="text-lg">{title}</p>
-          <p className="text-xl font-bold text-brand">{option}</p>
-          <p>${price}</p>
+          <p className="mt-1 text-sm text-gray-500">Size: {size}</p>
+          <p className="mt-2">${price}</p>
         </div>
-        <div className="flex items-center  text-2xl">
-          <AiOutlineMinusSquare onClick={handleMinus} className={ICON_CLASS} />
-          <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} className={ICON_CLASS} />
-          <RiDeleteBin5Fill onClick={handleDelete} className={ICON_CLASS} />
+        <div className="mt-4 flex justify-end md:m-0">
+          <div className="relative max-w-[66px]">
+            <select
+              value={selected}
+              onChange={(e) => handleSelect(e)}
+              className="relative w-full cursor-default appearance-none rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500 sm:text-sm sm:leading-6"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <ChevronDownIcon className="pointer-events-none absolute top-3 right-2 h-4 w-4 text-gray-300" />
+          </div>
+        </div>
+        <div className="mt-4 ml-8  flex items-center justify-end md:m-0 md:items-start">
+          <XMarkIcon
+            onClick={handleDelete}
+            className="hidden h-5 w-5 cursor-pointer text-gray-400  hover:text-gray-500 md:block "
+          />
+          <TrashIcon
+            onClick={handleDelete}
+            className="block h-5 w-5 cursor-pointer text-gray-400  hover:text-gray-500  md:hidden"
+          />
         </div>
       </div>
     </li>
