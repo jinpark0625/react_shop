217518e4ec4b1ce30053ec24421a2ab89437588c
jin park <jinpark0625@gmail.com>
2023-03-19 00:36:24 -0400

    feat: Add functionality for dynamic page titles and deduplication

--- src/pages/Product/Category.tsx
+++ src/pages/Product/Category.tsx
@@ -1,7 +1,38 @@
-import Products from '../../components/Products';
+import Products from './Products';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { CATEGORIES } from 'data/Products/Menus';
+import { removeCollectionsPrefix } from 'utils/collectionUtils';
 
 const Category = () => {
-  return <Products />;
+  const { pathname } = useLocation();
+  const pageTitle = removeCollectionsPrefix('/collections/', pathname);
+
+  const { type } = CATEGORIES[pageTitle] || { type: 'tags' };
+
+  const [pageData, setPageData] = useState({
+    key: type,
+    value: pageTitle,
+  });
+  const [shouldDeduplicate, setShouldDeduplicate] = useState(false);
+
+  useEffect(() => {
+    setPageData({
+      key: type,
+      value: pageTitle,
+    });
+    setShouldDeduplicate(false);
+  }, [pathname]);
+
+  return (
+    <>
+      <Products
+        pageData={pageData}
+        shouldDeduplicate={shouldDeduplicate}
+        setShouldDeduplicate={setShouldDeduplicate}
+      />
+    </>
+  );
 };
 
 export default Category;
