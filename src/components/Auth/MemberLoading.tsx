aa01bc254e0b37f197d7ab3606410a9dad7cd8e2
jin park <jinpark0625@gmail.com>
2023-03-20 18:53:59 -0400

    feat: Add Member page loading component

---
+++ src/components/Auth/MemberLoading.tsx
@@ -0,0 +1,16 @@
+const MemberLoading = () => {
+  return (
+    <div className="w-full">
+      <div className="mb-6 flex items-center">
+        <div className="h-12 w-12 animate-pulse rounded-full bg-gray-200"></div>
+        <div className="ml-5 h-[34px] w-[74px] animate-pulse rounded-lg bg-gray-200"></div>
+      </div>
+      <div className="mb-6 h-[58px] animate-pulse rounded-lg bg-gray-200"></div>
+      <div className="mb-6 h-[58px] animate-pulse rounded-lg bg-gray-200"></div>
+      <div className="mb-8 h-[58px] animate-pulse rounded-lg bg-gray-200"></div>
+      <div className="h-[44px] animate-pulse rounded-lg bg-gray-200"></div>
+    </div>
+  );
+};
+
+export default MemberLoading;
