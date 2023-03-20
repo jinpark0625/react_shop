5f8754f730e033ee053e3f417c61b4d350794db4
jin park <jinpark0625@gmail.com>
2023-03-20 19:18:48 -0400

    feat: Add file size limit and image input class data

--- src/data/Auth/authData.tsx
+++ src/data/Auth/authData.tsx
@@ -1,2 +1,9 @@
 export const EMAIL_REGEX =
   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+export const FILE_SIZE_MAX_LIMIT = 2 * 1024 * 1024;
+
+export const IMAGE_INPUT_CLASS = `block h-[58px] w-full rounded-r-lg text-sm text-slate-500
+file:mr-4 file:rounded-full file:border-0
+file:bg-violet-50 file:py-1 file:px-4 file:text-sm
+file:font-semibold file:text-violet-700 hover:file:bg-violet-100`;
