452746d56a3f65a96f12c4a8a23ea70a6fd0a7cd
jin park <jinpark0625@gmail.com>
2023-03-20 19:20:43 -0400

    design: Add z index to container

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from 'react-icons/cg';
 
 export default function Loading() {
   return (
-    <div className="relative z-10">
+    <div className="fixed z-50">
       <div className="fixed inset-0 bg-gray-500/20 transition-opacity">
         <div className="flex h-screen items-center justify-center">
           <CgSpinner className="animate-spin text-3xl text-indigo-600" />
