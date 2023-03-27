d7ccfce462170e1d621bf9e9362ad9defdb49858
jin park <jinpark0625@gmail.com>
2023-03-26 23:42:37 -0400

    feat: Add OrderLoading component

--- src/pages/Order/MyCart.tsx
+++ src/pages/Order/MyCart.tsx
@@ -2,12 +2,12 @@ import CartItem from '../../components/CartItem';
 import Button from '../../components/ui/Button';
 import useCart from '../../hooks/useCart';
 import ProductTitle from 'components/ui/ProductTitle';
-import LoadingSkeleton from 'components/ui/LoadingSkeleton';
 import { useAuthContext } from 'context/AuthContext';
 import NotFound from 'pages/NotFound';
 import { useNavigate } from 'react-router-dom';
 import ErrorMessage from 'components/ui/ErrorMessage';
 import OrderSummary from 'components/Order/OrderSummary';
+import OrderLoading from 'components/Order/OrderLoading';
 
 const MyCart = () => {
   const { ...contextData } = useAuthContext();
@@ -23,20 +23,8 @@ const MyCart = () => {
 
   if (!user) return <NotFound />;
 
-  if (isLoading)
-    return (
-      <div className="mx-auto mb-32 w-full max-w-7xl px-4 pt-24 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 lg:grid-cols-6">
-          <div className="col-span-4 mb-8 pr-0 lg:pr-10">
-            <LoadingSkeleton className=" animate-pulse rounded-md bg-slate-50  pt-[30%]" />
-            <LoadingSkeleton className="mt-10 animate-pulse rounded-md bg-slate-50  pt-[30%]" />
-          </div>
-          <div className="relative col-span-2 w-full animate-pulse overflow-hidden rounded-2xl bg-slate-50  pt-[100%]">
-            <LoadingSkeleton className="absolute top-0 left-0 h-full w-auto -translate-x-1/2 -translate-y-1/2 " />
-          </div>
-        </div>
-      </div>
-    );
+  if (isLoading) return <OrderLoading />;
+
   if (error)
     return (
       <div className="mx-auto max-w-7xl px-4 pt-24 text-center text-red-500 sm:px-6 lg:px-8">
