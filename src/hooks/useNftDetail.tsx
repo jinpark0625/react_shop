86d86ba6a83884aa63bbe13d7dc01d36fa97d3d4
jin park <jinpark0625@gmail.com>
2023-03-28 17:56:44 -0400

    feat: Add getCurrentEthereum function to get current Ethereum price
    - Currently, the Opensea API key is not available, so mockup data is used for the NFT data

--- src/hooks/useNftDetail.tsx
+++ src/hooks/useNftDetail.tsx
@@ -1,13 +1,18 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { NftType } from 'utils/interfaces';
 import { fetchSelectedNFT } from 'api/supabase';
-import opensea from 'api/opensea';
+import { getCurrentEthereum } from 'api/opensea';
+
+// interface ReturnDataType {
+//   data: NftType[];
+//   permalink: string;
+//   eth_price: number;
+//   usd_price: number;
+// }
 
 interface ReturnDataType {
   data: NftType[];
-  permalink: string;
-  eth_price: number;
-  usd_price: number;
+  usd: number;
 }
 
 export default function useNftDetail(id: string) {
@@ -15,17 +20,23 @@ export default function useNftDetail(id: string) {
     ['nft', id],
     async () => {
       const data = await fetchSelectedNFT(id);
+      // const {
+      //   data: {
+      //     permalink,
+      //     collection: {
+      //       // eslint-disable-next-line @typescript-eslint/naming-convention
+      //       payment_tokens: [{ eth_price, usd_price }],
+      //     },
+      //   },
+      // } = await opensea(data?.[0].token_id ?? '');
+      // return { data, permalink, eth_price, usd_price };
       const {
         data: {
-          permalink,
-          collection: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            payment_tokens: [{ eth_price, usd_price }],
-          },
+          ethereum: { usd },
         },
-      } = await opensea(data?.[0].token_id ?? '');
+      } = await getCurrentEthereum();
 
-      return { data, permalink, eth_price, usd_price };
+      return { data, usd };
     },
     {
       staleTime: 1000 * 60,
