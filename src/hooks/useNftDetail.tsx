a3ee24eeeb0d8901a43132b637a02f50668ee23a
jin park <jinpark0625@gmail.com>
2023-03-28 21:40:22 -0400

    feat: Add mockup data for NFT

--- src/hooks/useNftDetail.tsx
+++ src/hooks/useNftDetail.tsx
@@ -1,30 +1,42 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { NftType } from 'utils/interfaces';
 import { fetchSelectedNFT } from 'api/supabase';
-import { opensea } from 'api/opensea';
+// import { opensea } from 'api/opensea';
+import { getCurrentEthereum } from 'api/opensea';
+
+// interface ReturnDataType {
+//   data: NftType[];
+//   permalink: string;
+//   eth_price: number;
+//   usd_price: number;
+// }
 
 interface ReturnDataType {
   data: NftType[];
-  permalink: string;
-  eth_price: number;
-  usd_price: number;
+  usd: number;
 }
-
 export default function useNftDetail(id: string) {
   const selectedNftQuery: UseQueryResult<ReturnDataType, Error> = useQuery(
     ['nft', id],
     async () => {
       const data = await fetchSelectedNFT(id);
+      // const {
+      //   data: {
+      //     permalink,
+      //     collection: {
+      //       // eslint-disable-next-line @typescript-eslint/naming-convention
+      //       payment_tokens: [{ eth_price, usd_price }],
+      //     },
+      //   },
+      // } = await opensea(data?.[0].token_id ?? '');
+      // return { data, permalink, eth_price, usd_price };
       const {
         data: {
-          permalink,
-          collection: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            payment_tokens: [{ eth_price, usd_price }],
-          },
+          ethereum: { usd },
         },
-      } = await opensea(data?.[0].token_id ?? '');
-      return { data, permalink, eth_price, usd_price };
+      } = await getCurrentEthereum();
+
+      return { data, usd };
     },
     {
       staleTime: 1000 * 60,
