3edb311d235382c9f130b2a64e1282e008dcce80
jin park <jinpark0625@gmail.com>
2023-03-28 21:25:52 -0400

    feat: Add OPENSEA API KEY

--- src/api/opensea.ts
+++ src/api/opensea.ts
@@ -2,15 +2,17 @@
 import axios from 'axios';
 
 // Waiting for Opensea API Key
-// const baseURl = process.env.REACT_APP_NFT_API_URL!;
+const baseURl = process.env.REACT_APP_NFT_API_URL!;
 const contract = process.env.REACT_APP_CONTRACT_ID!;
+const headers = process.env.REACT_APP_OPENSEA_API_KEY!;
 
-const baseURl = process.env.REACT_APP_COINGECKO!;
+// const baseURl = process.env.REACT_APP_COINGECKO!;
 const CHAIN = 'ethereum';
 const CURRENCY = 'usd';
 
 const instance = axios.create({
   baseURL: baseURl,
+  headers: { 'X-API-KEY': headers },
   timeout: 1000,
 });
 
